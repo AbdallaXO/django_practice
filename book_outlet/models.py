28ddaa99d22512b7d392c7dc82eb608745388684
Artistic AD <aabouelnasr@mail.valenciacollege.edu>
2025-01-24 17:55:35 -0500

    added some more feautures

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
-    author = models.CharField(null=True, max_length=60)
+    author = models.CharField(null=True,max_length=60)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default=title, null=False)
+    
     
     def get_absolute_url(self):
-        return reverse("model_detail", kwargs={"pk": self.pk})
+        return reverse("book-detail", args=[self.id])
     
+    """
+    overwrides the save method and before we save, it makes for example abdalla akram abdalla-akram
+    """
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
     
     def __str__(self):
-        return f"{self.title} Written By Best Selling Author {self.author} Rating: {self.rating}"
+        return f"{self.title} Written By {self.author} Rating: {self.rating}"
     
     
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -5,10 +5,11 @@ from django.http import Http404
 # Create your views here.
 def index(request):
     books = Book.objects.all()
+    df = Book.objects.get(title__icontains="ab")
     return render(request, "book_outlet/index.html", {"books": books})
 
 def book_detail(request, id):
-
+    
     book = get_object_or_404(Book, pk=id) 
     return render(request, "book_outlet/book_detail.html", {
         "title":book.title,
