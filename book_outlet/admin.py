7c282d11c963ee1153a46ee5508ff21da450d9a4
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-01-25 16:04:56 -0500

    more updates and updated some on db

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("rating", "author")
+    list_display = ("title", "author", "rating")
+     
+    
+admin.site.register(Book, BookAdmin)
+    
 
 # Register your models here.
+
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,26 +4,23 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
-    author = models.CharField(null=True,max_length=60)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default=title, null=False)
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
     
     
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])
+        return reverse("book-detail", args=[self.slug])
     
-    """
-    overwrides the save method and before we save, it makes for example abdalla akram abdalla-akram
-    """
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-        
     
     def __str__(self):
-        return f"{self.title} Written By {self.author} Rating: {self.rating}"
+        return f"{self.title} Written By {self.author.first_name} Rating: {self.rating}"
     
     
