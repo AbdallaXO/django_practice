dc5c0c0df867200ca1e0824fbc59da3461ab54bf
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-01-25 16:46:30 -0500

    Added new models, address with one to one relationship to the author and modified its meta data

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
@@ -8,6 +8,9 @@ class BookAdmin(admin.ModelAdmin):
      
     
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+
     
 
 # Register your models here.
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,35 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    zip_code = models.CharField(max_length=6)
+    city = models.CharField(max_length=20)
+    
+    def full_address(self):
+        return f"\n{self.street} \n{self.city} \n{self.zip_code}"
+    
+    def __str__(self):
+        return self.full_address()
+    """ To manipualte how the model is viewed basically name of model or something
+    since we cant do that with variables only with meta data."""
+    class Meta:
+        verbose_name_plural = "Address"
+        
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    
+    def full_name(self):
+        return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
+    
+    def __str__(self):
+        return self.full_name()
+    
+    
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
